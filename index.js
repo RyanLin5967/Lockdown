defcbfb504f20820a276abc64108de5973e4ab90
RyanLin5967 <idident5967@gmail.com>
2025-04-26 12:41:37 -0400

    other page made

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const playButton = document.getElementById('playbtn');
-const canvas = document.getElementById('canvas1');
 const mainpage = document.getElementById('mainpage');
-const ctx = canvas.getContext('2d');
 
-function resizeCanvas() {
-    canvas.style.display = 'block';
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    mainpage.style.display = 'none';
-  }
+var myGamePiece;
+var myGamePiece;
+
+function toGame(){
+
+}
+function startGame() {
+    myGameArea.start();
+    myGamePiece = new component(30, 30, "red", 10, 120);
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        window.addEventListener('keydown', keyDownHandler);
+        window.addEventListener('keyup', keyUpHandler);
+    },
+    clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+}
+};
+
+function component(width, height, color, x, y) {
+  this.speedY = 0;
+  this.speedX = 0;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;    
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+}
