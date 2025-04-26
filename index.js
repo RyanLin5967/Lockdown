4252f2972cfb2aa37b55fe68e587282bdd741234
Anthony Su <anthonysu836@gmail.com>
2025-04-26 11:19:56 -0400

    added gameselectors

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const canvas = document.getElementById('canvas1');
 const mainpage = document.getElementById('mainpage');
 const ctx = canvas.getContext('2d');
 
+const gmae1 = document.getElementById('game1');
+const gmae2 = document.getElementById('game2');
+const gmae3 = document.getElementById('game3');
+
 function resizeCanvas() {
-    canvas.style.display = 'block';
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    mainpage.style.display = 'none';
+    gmae1.style.display = 'block';
+    gmae2.style.display = 'block';
+    gmae3.style.display = 'block';
+    playButton.style.display = 'none';
   }
+
