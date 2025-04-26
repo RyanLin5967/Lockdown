ec8da484cef271f1776fe3f15263c3c4fdf5ac50
Anthony Su <anthonysu836@gmail.com>
2025-04-26 10:59:54 -0400

    CHanegs

---
+++ index.js
@@ -0,0 +1,6 @@
+const playButton = document.getElementById('playbtn');
+const canvas = document.getElementById('canvas1');
+const mainpage = document.getElementById('mainpage');
+const ctx = canvas.getContext('2d');
+
+
