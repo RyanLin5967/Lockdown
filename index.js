22ecc7d815af06a488dae3e4f8ed373c12798a20
Anthony Su <anthonysu836@gmail.com>
2025-04-26 11:09:54 -0400

    fixed play button

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const canvas = document.getElementById('canvas1');
 const mainpage = document.getElementById('mainpage');
 const ctx = canvas.getContext('2d');
 
-
+function resizeCanvas() {
+    canvas.style.display = 'block';
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    mainpage.style.display = 'none';
+  }
